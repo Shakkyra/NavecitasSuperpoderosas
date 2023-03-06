bb93096db79a1d1323f224a713d488ac74d8ee5b
Shakkyra <86767990+Shakkyra@users.noreply.github.com>
2023-03-06 12:43:45 -0600

    SpawnEnemigos
    Los enemigos spawnean de forma aleatoria desde la pared.

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -7,6 +7,8 @@ public class Spawn : MonoBehaviour
 {
     [SerializeField]
     private Enemigo _enemigoOriginal;
+    [SerializeField]
+    private GameObject _self;
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +20,21 @@ public class Spawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Comienzo")){
+        /*if(Input.GetButtonDown("Comienzo")){
             Instantiate(
                 _enemigoOriginal,
                 transform.position,
                 transform.rotation
             );
+        }*/
+        if(Input.GetButtonDown("Comienzo")){
+            Vector3 posicionRectangulo = _self.transform.position;
+            //Dividimos la pared en dos
+            float mitadRect = _self.transform.localScale.x / 2f;
+            //Elegimos una parte aleatoria
+            float posAleatoria = Random.Range(posicionRectangulo.x - mitadRect, posicionRectangulo.x + mitadRect);
+            Vector3 posicionSpawn = new Vector3(posAleatoria,posicionRectangulo.y,posicionRectangulo.z);
+            Instantiate(_enemigoOriginal,posicionSpawn,Quaternion.identity);
         }
         
     }
